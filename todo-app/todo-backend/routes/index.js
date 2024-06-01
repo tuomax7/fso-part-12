b9fd9101397caabb78ea583e9c467f13e911dcc9
Tuomas Nummela <tuomas.nummela.per@gmail.com>
2024-06-01 13:42:17 +0300

    exercises 12.7 - 12.12

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,17 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const configs = require('../util/config')
+const redis = require("../redis");
 
-let visits = 0
+const configs = require("../util/config");
+
+let visits = 0;
 
 /* GET index data. */
-router.get('/', async (req, res) => {
-  visits++
+router.get("/", async (req, res) => {
+  visits++;
 
   res.send({
     ...configs,
-    visits
+    visits,
+  });
+});
+
+/* GET statistics data. */
+router.get("/statistics", async (req, res) => {
+  if (!(await redis.getAsync("addedTodos"))) {
+    await redis.setAsync("addedTodos", 0);
+  }
+
+  const addedTodos = await redis.getAsync("addedTodos");
+
+  res.send({
+    addedTodos: Number(addedTodos),
   });
 });
 
